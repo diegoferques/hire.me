153f6efb28d8597ca8b2f05e6b8dd4297f6b98f7
diegoferques <diegoferques@gmail.com>
2017-03-13 12:52:17 -0300

    Commit com TESTE implementado

--- src/test/java/com/bemobi/UrlShortenerApplicationTests.java
+++ src/test/java/com/bemobi/UrlShortenerApplicationTests.java
@@ -2,15 +2,30 @@ package com.bemobi;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UrlShortenerApplicationTests {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	public void contextLoads() {
+		String body = this.restTemplate.getForObject("/u/vaga", String.class);
+		assertThat(body).isEqualTo("Hello World");
+	}
+
+	@Test
+	public void testError002() {
+		String body = this.restTemplate.getForObject("/u/vagaErrada", String.class);
+		assertThat(body).isEqualTo("{\"errorCode\":\"002\",\"description\":\"SHORTENED URL NOT FOUND\",\"alias\":\"vagaErrada\"}");
 	}
 
 }
