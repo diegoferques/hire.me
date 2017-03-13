4c1f2f0d3f9c7c0b25569e1e6c9a14e1a7fda9c4
diegoferques <diegoferques@gmail.com>
2017-03-13 12:48:53 -0300

    Commit com shortnerService implementado utilizando a Biblioteca de Hash do Google

--- src/main/java/com/bemobi/service/ShortenerService.java
+++ src/main/java/com/bemobi/service/ShortenerService.java
@@ -1,11 +1,18 @@
 package com.bemobi.service;
 
+import com.google.common.hash.Hashing;
 import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
 
 @Service
 public class ShortenerService {
 
     public String shortenerUrl(String url) {
+        //BIBLIOTECA DO GOOGLE
+        //https://google.github.io/guava/releases/19.0/api/docs/com/google/common/hash/Hashing.html
+        String hashedUrl = Hashing.murmur3_32().hashString(url, StandardCharsets.UTF_8).toString();
+
         return String.valueOf(System.nanoTime());
     }
 
